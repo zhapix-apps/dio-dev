220cb8dac45e039294b87ed997ed5912c7f5e35c
Vijayan T <69061573+vijayancpt@users.noreply.github.com>
2026-07-10 18:39:48 +0530

    Updates

---
+++ _next/static/opKKEAWkKk26U6lUgvDyu/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dio-dev//_next/:path+",
+        "destination": "/dio-dev/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
